16cb41986a89645f3c8ef5c990f757a5bb5c1476
y-yj-lim <ases3838evan0525@gmail.com>
2025-12-07 16:55:39 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+<script>
+        const firebaseConfig = {
+            apiKey: "AIzaSyA6-B7HkV2oaAfDeQaZy53VbkBglbB7mRU",
+            authDomain: "study-addict.firebaseapp.com",
+            projectId: "study-addict",
+            storageBucket: "study-addict.firebasestorage.app",
+            messagingSenderId: "345011206990",
+            appId: "1:345011206990:web:1f8aa1b3cb683fd2a87c1f",
+            measurementId: "G-1VZ7MBZQW1"
+        };
+    </script>
